c9426954c0e4c489b80a8572ffcb97f57d37e1a0
Ibrahim Brarou <ibrahim.brarou@gmail.com>
2022-09-03 13:27:21 +0200

    Added all Models.

---
+++ FilmTickets/Data/MovieCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action=1,
+        Comdey,
+        Drama,
+        Documentary
+    }
+}
